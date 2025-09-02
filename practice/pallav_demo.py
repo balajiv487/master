22d41888040b3d54053176e231aae3110e7c45a5
balajv487 <balajiv487@gmail.com>
2025-09-02 10:37:57 -0400

    demo

---
+++ practice/pallav_demo.py
@@ -0,0 +1,1 @@
+print("Hi Pallav\nHow are you")
--- practice/pr9.py
+++ practice/pr9.py
@@ -2,8 +2,10 @@ from enum import Enum
 class Color(Enum):
     red=1
     green=2
-    blue=3
+    blue=4
 print(Color.red.name)
 print(Color.red.value)
 for i in Color:
     print(i)
+
+#This is for testing purpose
