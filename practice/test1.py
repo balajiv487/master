fe465fa09462c7e82f44a79b5bcbae7a4bad5bb7
balajv487 <balajiv487@gmail.com>
2025-10-01 18:57:55 -0400

    testing

--- practice/test1.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter a number to check prime or not:"))
-for i in range(2,n):
-    print(i)
-    if n%i==0:
-        print("Given number is not prime")
-        break
-else:
-    print("Give number is a prime number")
